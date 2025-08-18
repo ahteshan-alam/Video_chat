e8ef0de6098bb3c628d0c0722700d62961b7bfd4
Ahteshan <mdahteshan796@gmail.com>
2025-08-19 00:52:08 +0530

    adapter

--- frontend/src/home/home.jsx
+++ frontend/src/home/home.jsx
@@ -1,4 +1,4 @@
-import 'webrtc-adapter'; 
+
 import { useEffect, useRef, useState } from 'react';
 import { useLocation, useNavigate } from 'react-router-dom';
 import './home.css'
@@ -8,17 +8,6 @@ const configuration = {
   iceServers: [
     { urls: 'stun:stun.l.google.com:19302' },
     { urls: 'stun:stun1.l.google.com:19302' },
-   
-    {
-      urls: "turn:openrelay.metered.ca:80",
-      username: "openrelayproject",
-      credential: "openrelayproject",
-    },
-    {
-      urls: "turn:openrelay.metered.ca:443",
-      username: "openrelayproject",
-      credential: "openrelayproject",
-    }
   ],
 };
 
