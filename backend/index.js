7b1967dd7bd6bd608fd47d45877f81bdc564a65d
Ahteshan <mdahteshan796@gmail.com>
2025-08-18 22:05:35 +0530

    back

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const server = createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "https://videochater.netlify.app",
         methods: ["GET", "POST"]
     }
 })
